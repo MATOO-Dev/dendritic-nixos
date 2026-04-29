f10336b29095552ed2d026ad33b3b28d4ce81526
MATOO-Dev <matoo-dev@proton.me>
2026-04-29 15:32:56 +0200

    WIP fixing syntax

--- modules/niri/niri.nix
+++ modules/niri/niri.nix
@@ -1,14 +1,18 @@
 {
-	perSystem.packages.my-niri = {
+	perSystem = {
 		inputs,
 		pkgs,
+		lib,
 		...
-	}:
+	}: {
+packages.niri = {
 		inputs.wrapper-modules.wrappers.niri.wrap {
 			inherit pkgs;
 
 			settings = {
 				# niri settings go here, or use modules
+				binds."Mod+Q".spawn-sh = lib.getExe pkgs.kitty;
 			};
 		};
+		}
 }
