14148766b079acd3a5046afb42a4bc4e32f1df64
MATOO-Dev <matoo-dev@proton.me>
2026-05-03 00:23:41 +0200

    Commented disk modules for laptop and desktop

--- modules/hosts/laptop/laptop-disk.nix
+++ modules/hosts/laptop/laptop-disk.nix
@@ -11,90 +11,91 @@ let
     ];
 in
 {
-    flake.diskoConfigurations.traveler = {
-        imports = [
-            inputs.disko.nixosModules.default
-        ];
-
-        disko.devices = {
-            disk = {
-                primary = {
-                    type = "disk";
-                    device = primary_disk_path;
-                    content = {
-                        type = "gpt";
-                        partitions = {
-                            esp = {
-                                name = "ESP";
-                                size = "1G";
-                                type = "EF00";
-                                content = {
-                                    type = "filesystem";
-                                    format = "vfat";
-                                    mountpoint = "/boot";
-                                    mountOptions = [ "umask=00777" ];
-                                };
-                            };
-                            encrypedSwap = {
-                                size = "8G";
-                                content = {
-                                    type = "swap";
-                                    # mutually exclusive with resumeDevice
-                                    randomEncryption = true;
-                                };
-                            };
-                            luks = {
-                                size = "100%";
-                                content = {
-                                    type = "luks";
-                                    name = "cryptroot";
-                                    settings = {
-                                        # discard means unused blocks are zeroed
-                                        # the data is still encrypted, but one can
-                                        # see which parts & how much of the disk is used
-                                        allowDiscards = true;
-                                    };
-                                    content = {
-                                        type = "btrfs";
-                                        extraArgs = [ "-f" ];
-                                        subvolumes = {
-                                            "@" = {
-                                                mountpoint = "/";
-                                                mountOptions = mount_options;
-                                            };
-                                            "@home" = {
-                                                mountpoint = "/home";
-                                                mountOptions = mount_options;
-                                            };
-                                            "@nix" = {
-                                                mountpoint = "/nix";
-                                                mountOptions = mount_options;
-                                            };
-                                            "@var_log" = {
-                                                mountpoint = "/var/log";
-                                                mountOptions = mount_options;
-                                            };
-                                            "@snapshots" = {
-                                                mountpoint = "/.snapshots";
-                                                mountOptions = mount_options;
-                                            };
-                                            "@tmp" = {
-                                                mountpoint = "/tmp";
-                                                mountOptions = mount_options;
-                                            };
-                                            # on laptop this is on primary disk
-                                            "@games" = {
-                                                mountpoint = "/home/matoo/Games";
-                                                mountOptions = mount_options;
-                                            };
-                                        };
-                                    };
-                                };
-                            };
-                        };
-                    };
-                };
-            };
-        };
-    };
+	# TODO: fix dobule
+    # flake.diskoConfigurations.traveler = {
+    #     imports = [
+    #         inputs.disko.nixosModules.default
+    #     ];
+    #
+    #     disko.devices = {
+    #         disk = {
+    #             primary = {
+    #                 type = "disk";
+    #                 device = primary_disk_path;
+    #                 content = {
+    #                     type = "gpt";
+    #                     partitions = {
+    #                         esp = {
+    #                             name = "ESP";
+    #                             size = "1G";
+    #                             type = "EF00";
+    #                             content = {
+    #                                 type = "filesystem";
+    #                                 format = "vfat";
+    #                                 mountpoint = "/boot";
+    #                                 mountOptions = [ "umask=00777" ];
+    #                             };
+    #                         };
+    #                         encrypedSwap = {
+    #                             size = "8G";
+    #                             content = {
+    #                                 type = "swap";
+    #                                 # mutually exclusive with resumeDevice
+    #                                 randomEncryption = true;
+    #                             };
+    #                         };
+    #                         luks = {
+    #                             size = "100%";
+    #                             content = {
+    #                                 type = "luks";
+    #                                 name = "cryptroot";
+    #                                 settings = {
+    #                                     # discard means unused blocks are zeroed
+    #                                     # the data is still encrypted, but one can
+    #                                     # see which parts & how much of the disk is used
+    #                                     allowDiscards = true;
+    #                                 };
+    #                                 content = {
+    #                                     type = "btrfs";
+    #                                     extraArgs = [ "-f" ];
+    #                                     subvolumes = {
+    #                                         "@" = {
+    #                                             mountpoint = "/";
+    #                                             mountOptions = mount_options;
+    #                                         };
+    #                                         "@home" = {
+    #                                             mountpoint = "/home";
+    #                                             mountOptions = mount_options;
+    #                                         };
+    #                                         "@nix" = {
+    #                                             mountpoint = "/nix";
+    #                                             mountOptions = mount_options;
+    #                                         };
+    #                                         "@var_log" = {
+    #                                             mountpoint = "/var/log";
+    #                                             mountOptions = mount_options;
+    #                                         };
+    #                                         "@snapshots" = {
+    #                                             mountpoint = "/.snapshots";
+    #                                             mountOptions = mount_options;
+    #                                         };
+    #                                         "@tmp" = {
+    #                                             mountpoint = "/tmp";
+    #                                             mountOptions = mount_options;
+    #                                         };
+    #                                         # on laptop this is on primary disk
+    #                                         "@games" = {
+    #                                             mountpoint = "/home/matoo/Games";
+    #                                             mountOptions = mount_options;
+    #                                         };
+    #                                     };
+    #                                 };
+    #                             };
+    #                         };
+    #                     };
+    #                 };
+    #             };
+    #         };
+    #     };
+    # };
 }
