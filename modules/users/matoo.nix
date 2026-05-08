f5297be38448c1418051ae31f75b46ef3c568f4c
MATOO-Dev <matoo-dev@proton.me>
2026-05-08 14:18:00 +0200

    Added neovim module to home definition

--- modules/users/matoo.nix
+++ modules/users/matoo.nix
@@ -15,9 +15,9 @@
     };
 
     flake.homeConfigurations.matoo = inputs.home-manager.lib.homeManagerConfiguration {
-		# TODO: figure out how to set this automatically
-		# may also be useful for secondary pkgs-unstable
-		pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+        # TODO: figure out how to set this automatically
+        # may also be useful for secondary pkgs-unstable
+        pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
         modules = with self.homeModules; [
             bar
             coding
@@ -30,7 +30,7 @@
             kitty
             matoo
             messaging
-            # neovim
+            neovim
             # niri
             nix
             prompt
@@ -58,7 +58,7 @@
                 stateVersion = "25.11";
             };
 
-			# mut home manager news
-			news.display = "silent";
+            # mut home manager news
+            news.display = "silent";
         };
 }
