3a344929d67eaae7f5d5c2325bf38b3fa439dfa0
MATOO-Dev <matoo-dev@proton.me>
2026-05-08 14:18:55 +0200

    Fixed syntax

--- modules/neovim/nvim.nix
+++ modules/neovim/nvim.nix
@@ -1,39 +1,42 @@
 {
-	inputs,
-	self,
-	...
-}: {
-	flake.modules.home.neovim = {
-		home.packages = with self.packages; [nvim-full];
-	};
+    inputs,
+    self,
+    ...
+}:
+{
+    flake.homeModules.neovim = {
+        home.packages = with self.packages; [ nvim-full ];
+    };
 
-	perSystem = {pkgs, ...}: {
-		packages.nvim-full =
-			(inputs.nvf.lib.neovimConfiguration {
-					inherit pkgs;
-					modules = with self.modules.nvim; [
-						autocomplete
-						binds
-						debugging
-						diagnostics
-						git
-						languages
-						navigation
-						options
-						theme
-						typing
-						visuals
-					];
-				}).neovim;
+    perSystem =
+        { pkgs, ... }:
+        {
+            packages.nvim-full =
+                (inputs.nvf.lib.neovimConfiguration {
+                    inherit pkgs;
+                    modules = with self.modules.nvim; [
+                        autocomplete
+                        binds
+                        debugging
+                        diagnostics
+                        git
+                        languages
+                        navigation
+                        options
+                        theme
+                        typing
+                        visuals
+                    ];
+                }).neovim;
 
-		packages.nvim-small =
-			(inputs.nvf.lib.neovimConfiguration {
-					inherit pkgs;
-					modules = with self.modules.nvim; [
-						binds
-						navigation
-						options
-					];
-				}).neovim;
-	};
+            packages.nvim-small =
+                (inputs.nvf.lib.neovimConfiguration {
+                    inherit pkgs;
+                    modules = with self.modules.nvim; [
+                        binds
+                        navigation
+                        options
+                    ];
+                }).neovim;
+        };
 }
