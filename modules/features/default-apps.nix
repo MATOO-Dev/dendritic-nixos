34f8bdc17b28333339d92e9968b853c1b1366244
MATOO-Dev <matoo-dev@proton.me>
2026-05-13 10:26:03 +0200

    Fixed typo in PDF mime type

--- modules/features/default-apps.nix
+++ modules/features/default-apps.nix
@@ -1,26 +1,24 @@
 {
-    flake.homeModules.defaultApps =
-        { pkgs, ... }:
-        {
-            home.packages = [
-                # requires file input to work
-                # test if syntax is correct like this
-                (pkgs.writeShellScriptBin "find-mime-type" ''
-                    					${pkgs.file}/bin/file --mime-type -b
-                    				'')
-            ];
+	flake.homeModules.defaultApps = {pkgs, ...}: {
+		home.packages = [
+			# requires file input to work
+			# test if syntax is correct like this
+			(pkgs.writeShellScriptBin "find-mime-type" ''
+					${pkgs.file}/bin/file --mime-type -b
+				'')
+		];
 
-            xdg.mime.enable = true;
-            xdg.mimeApps = {
-                enable = true;
-                defaultApplications = {
-                    "applications/pdf" = "evince.desktop";
-                    "image/*" = "eog.desktop";
-                    "x-scheme-handler/http" = "firefox.desktop";
-                    "x-scheme-handler/https" = "firefox.desktop";
-                    "x-scheme-handler/about" = "firefox.desktop";
-                    "x-scheme-handler/unknown" = "firefox.desktop";
-                };
-            };
-        };
+		xdg.mime.enable = true;
+		xdg.mimeApps = {
+			enable = true;
+			defaultApplications = {
+				"application/pdf" = "evince.desktop";
+				"image/*" = "eog.desktop";
+				"x-scheme-handler/http" = "firefox.desktop";
+				"x-scheme-handler/https" = "firefox.desktop";
+				"x-scheme-handler/about" = "firefox.desktop";
+				"x-scheme-handler/unknown" = "firefox.desktop";
+			};
+		};
+	};
 }
