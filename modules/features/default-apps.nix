fbf250eeaf92e62822a03ebd2b3ab2e8d74be88d
MATOO-Dev <matoo-dev@proton.me>
2026-05-13 15:19:47 +0200

    Started writing find-desktop-filtered to find .desktop files

--- modules/features/default-apps.nix
+++ modules/features/default-apps.nix
@@ -8,6 +8,22 @@
 					runtimeInputs = with pkgs; [file];
 					text = ''file --mime-type -b "$1"'';
 				})
+			(pkgs.writeShellApplication {
+					name = "find-desktop-filtered";
+					runtimeInputs = with pkgs; [findutils ripgrep];
+					text = ''
+						# global packages
+						# find /run/current-system/sw/share/applications | rg -i "$1"
+						# user packages
+						# find /etc/profiles/per-user/"$(id -n -u)"/share/applications | rg -i "$1"
+						# home manager packages
+						find ~/.nix-profile/share/applications | rg -i "$1"
+						# global flatpaks
+						# find /var/lib/flatpak/exports/share/applications | rg -i "$1"
+						# user flatpaks
+						# find ~/.local/share/flatpak/exports/share/applications | rg -i "$1"
+					'';
+				})
 		];
 
 		xdg.mime.enable = true;
