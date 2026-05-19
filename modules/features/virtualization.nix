ae39837f75a67f964c1e963426e65ebe01a88bb6
MATOO-Dev <matoo-dev@proton.me>
2026-05-19 14:06:10 +0200

    Updated virtualization module

--- modules/features/virtualization.nix
+++ modules/features/virtualization.nix
@@ -1,8 +1,12 @@
 {
-	flake.nixosModules.virtualization = {
+	flake.nixosModules.virtualization = {pkgs, ...}: {
 		programs.virt-manager.enable = true;
 
 		virtualisation.libvirtd.enable = true;
+
+		environment.systemPackages = with pkgs; [
+			dnsmasq
+		];
 
 		# TODO: apply to all users
 		users.users.matoo.extraGroups = [
