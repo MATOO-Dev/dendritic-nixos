4eb9223bbab4bbf9148dd43582db99cfbd2fe1e7
MATOO-Dev <matoo-dev@proton.me>
2026-05-20 15:45:54 +0200

    Moved general.nix to nix.nix

--- modules/features/nix.nix
+++ modules/features/nix.nix
@@ -1,46 +1,69 @@
-{ inputs, ... }:
-{
-    flake.nixosModules.nix = {
-        nix = {
-            settings.experimental-features = [
-                "nix-command"
-                "flakes"
-            ];
-        };
+{inputs, ...}: {
+	imports = [
+		inputs.flake-parts.flakeModules.modules
+		inputs.home-manager.flakeModules.home-manager
+		inputs.wrapper-modules.flakeModules.wrappers
+		inputs.disko.flakeModules.default
+	];
 
-        nixpkgs.config = {
-            allowUnfree = true;
-            system = "x86_64-linux";
-        };
+	systems = [
+		"x86_64-linux"
+	];
 
-        # nix helper
-        programs.nh = {
-            enable = true;
-            clean.enable = true;
-            clean.extraArgs = "--keep 5 --keep-since 7d";
-            flake = "/home/matoo/dendritic-nixos";
-        };
+	perSystem = {system, ...}: {
+		# _module.args.pkgs =
+		# 	import inputs.nixpkgs.legacyPackages {
+		# 		inherit system;
+		# 		config.allowUnfree = true;
+		# 	};
+		# _module.args.pkgs-unstable =
+		# 	import inputs.nixpkgs-unstable.legacyPackages {
+		# 		inherit system;
+		# 		config.allowUnfree = true;
+		# 	};
+	};
 
-        # # automatic updates
-        # system.autoUpgrade = {
-        #     enable = false;
-        #     allowReboot = false;
-        #     flake = inputs.self.outPath;
-        #     flags = [
-        #         "--print-build-logs"
-        #     ];
-        #     dates = "06:00";
-        #     randomizedDelaySec = "15min";
-        #     channel = inputs.flake.nixpkgs;
-        # };
-    };
+	flake.nixosModules.nix = {
+		nix = {
+			settings.experimental-features = [
+				"nix-command"
+				"flakes"
+			];
+		};
 
-    flake.homeModules.nix = {
-    	imports = [
-    		inputs.nix-index-database.homeModules.default
-    	];
-    	nixpkgs.config = {
-    		allowUnfree = true;
-    	};
-    };
+		nixpkgs.config = {
+			allowUnfree = true;
+			system = "x86_64-linux";
+		};
+
+		# nix helper
+		programs.nh = {
+			enable = true;
+			clean.enable = true;
+			clean.extraArgs = "--keep 5 --keep-since 7d";
+			flake = "/home/matoo/dendritic-nixos";
+		};
+
+		# # automatic updates
+		# system.autoUpgrade = {
+		#     enable = false;
+		#     allowReboot = false;
+		#     flake = inputs.self.outPath;
+		#     flags = [
+		#         "--print-build-logs"
+		#     ];
+		#     dates = "06:00";
+		#     randomizedDelaySec = "15min";
+		#     channel = inputs.flake.nixpkgs;
+		# };
+	};
+
+	flake.homeModules.nix = {
+		imports = [
+			inputs.nix-index-database.homeModules.default
+		];
+		nixpkgs.config = {
+			allowUnfree = true;
+		};
+	};
 }
