41ca49521b29d5e13bffb4b155913e796707d101
MATOO-Dev <matoo-dev@proton.me>
2026-05-23 17:45:10 +0200

    Ported hardware config for desktop

--- modules/hosts/desktop/desktop-conf.nix
+++ modules/hosts/desktop/desktop-conf.nix
@@ -1,51 +1,67 @@
-{ inputs, self, ... }:
 {
-    # TODO: name
-    flake.nixosConfigurations.desktop = inputs.nixpkgs.lib.nixosSystem {
-        modules =
-            with self.nixosModules;
-            [
-                audio
-                bluetooth
-                bootloader
-                compatibility
-                email
-                extraPackages
-                fonts
-                gaming
-                graphics
-                input
-                kernel
-                locale
-                localsend
-                login
-                matoo
-                messaging
-                networking
-                niri
-                nix
-                printing
-                snapshots
-				virtualization
-				# debug
-				flatpak
-            ]
-            ++ [
-                # TODO: name
-                self.nixosModules.desktop
-                self.diskoConfigurations.desktop
-            ];
-    };
+	inputs,
+	self,
+	...
+}: {
+	# TODO: name
+	flake.nixosConfigurations.desktop =
+		inputs.nixpkgs.lib.nixosSystem {
+			modules = with self.nixosModules;
+				[
+					audio
+					bluetooth
+					bootloader
+					compatibility
+					email
+					extraPackages
+					fonts
+					gaming
+					graphics
+					input
+					kernel
+					locale
+					localsend
+					login
+					matoo
+					messaging
+					networking
+					niri
+					nix
+					printing
+					snapshots
+					virtualization
+					# debug
+					flatpak
+				]
+				++ [
+					# TODO: name
+					self.nixosModules.desktop
+					self.diskoConfigurations.desktop
+				];
+		};
 
-    # TODO: name
-    flake.nixosModules.desktop = {
-        # TODO: name
-        networking.hostname = "desktop";
-        system.stateVersion = "25.11";
+	# TODO: name
+	flake.nixosModules.desktop = {
+		# TODO: name
+		networking.hostname = "desktop";
+		system.stateVersion = "25.11";
 
+		# hardware config
 		nixpkgs.hostPlatform = "x86_64-linux";
-
-        # TODO: hardware config
-        # hardware config
-    };
+		boot = {
+			initrd.availableKernelModules = [
+				"nvme"
+				"xhci_pci"
+				"ahci"
+				"usb_storage"
+				"usbhid"
+				"sd_mod"
+			];
+			initrd.kernelModules = [];
+			kernelModules = ["kvm-amd"];
+			extraModulePackages = [];
+		};
+		hardware.enableRedistributableFirmware = true;
+		hardware.cpu.amd.updateMicrocode = true;
+	};
 }
