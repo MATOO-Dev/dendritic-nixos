20b5138f5d81e65dc38b8abc2eaaad6e67078a11
MATOO-Dev <matoo-dev@proton.me>
2026-05-23 14:40:48 +0200

    Switched kernel

--- modules/features/kernel.nix
+++ modules/features/kernel.nix
@@ -1,6 +1,7 @@
 {
 	flake.nixosModules.kernel = {pkgs, ...}: {
 		# use most recent available kernel
-		boot.kernelPackages = pkgs.unstable.linuxPackages;
+		# boot.kernelPackages = pkgs.unstable.linuxPackages;
+		boot.kernelPackages = pkgs.linuxPackages;
 	};
 }
