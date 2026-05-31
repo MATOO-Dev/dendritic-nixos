4005a64f854f7014d48a3d12d6a7c49f471a1a34
MATOO-Dev <matoo-dev@proton.me>
2026-05-31 12:48:25 +0200

    Disabled firefox picture-in-picture video player

--- modules/firefox/firefox-settings.nix
+++ modules/firefox/firefox-settings.nix
@@ -1,8 +1,9 @@
 {
 	flake.homeModules.firefox = {
 		programs.firefox.profiles.matoo.settings = {
-			"media.eme.enabled" = true;
-			"media.gmp-widevinecdm.enabled" = true;
+			"media.eme.enabled" = true; # watch DRM content
+			"media.gmp-widevinecdm.enabled" = true; # watch DRM content
+			"media.videocontrols.picture-in-picture.enabled" = false; # disable picture-in-picture
 		};
 	};
 }
