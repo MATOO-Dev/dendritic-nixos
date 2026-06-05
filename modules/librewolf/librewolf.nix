358976117998bfafca7df1b15a09ed5649cc1856
MATOO-Dev <matoo-dev@proton.me>
2026-06-05 11:51:33 +0200

    Edited desktop file for librewolf to include firefox keyword

--- modules/librewolf/librewolf.nix
+++ modules/librewolf/librewolf.nix
@@ -1,17 +1,42 @@
 {self, ...}: {
-	flake.homeModules.librewolf = {
+	flake.homeModules.librewolf = {pkgs, ...}: {
 		programs.librewolf = {
 			enable = true;
 			languagePacks = ["en-US"];
 		};
-	};
 
-	# flake.homeModules.firefox = {
-	# 	modules = with self.modules.firefox; [
-	# 		addons
-	# 		general
-	# 		policies
-	# 		profiles
-	# 	];
-	# };
+		# add firefox to keywords for .desktop entry
+		xdg.desktopEntries.librewolf = {
+			categories = ["Network" "WebBrowser"];
+			exec = "librewolf --name librewolf %U";
+			genericName = "Web Browser";
+			icon = "librewolf";
+			# mimeType = [
+			# 	"text/html"
+			# 	"text/xml"
+			# 	"application/xhtml+xml"
+			# 	"application/vnd.mozilla.xul+xml"
+			# ];
+			name = "LibreWolf";
+			startupNotify = true;
+			terminal = false;
+			type = "Application";
+			settings = {
+				StartupWMClass = "librewolf";
+				Version = "1.5";
+				Keywords = "web;browser;internet;firefox";
+			};
+
+			actions = {
+				"new-window" = {
+					name = "New Window";
+					exec = "librewolf --new-window %U";
+				};
+				"new-private-window" = {
+					name = "New Private Window";
+					exec = "librewolf --private-window %U";
+				};
+			};
+		};
+	};
 }
