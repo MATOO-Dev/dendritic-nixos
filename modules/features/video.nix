7ab4814d6401cd9c1896a435f18f81a9a007f04c
MATOO-Dev <matoo-dev@proton.me>
2026-07-04 12:21:17 +0200

    Added some configuration for mpv

--- modules/features/video.nix
+++ modules/features/video.nix
@@ -1,40 +1,55 @@
 {
 	flake.homeModules.video = {pkgs, ...}: {
-		home.packages = with pkgs; [
-			footage # trimmer
-			kdePackages.kdenlive # editor
-			vlc # player
-			yt-dlg # download graphically
-			yt-dlp # download from cli
-			# player + plugins
-			(mpv.override {
-					scripts = with mpvScripts; [
-						cut
-					];
-				})
-		];
+		home.packages = let
+			mpv-replace =
+				pkgs.writeShellApplication {
+					name = "mpv-replace";
+					runtimeInputs = with pkgs; [mpv socat];
+					text = ''
+						SOCKET="/run/user/$(id -u)/mpv-socket"
+						if [ -S "$SOCKET" ]; then
+							printf '{"command":["loadfile","%s","replace"]}\n' "$1" | socat - "$SOCKET"
+						else
+							mpv --input-ipc-server="$SOCKET" "$@"
+						fi
+					'';
+				};
+		in
+			with pkgs; [
+				footage # trimmer
+				kdePackages.kdenlive # editor
+				vlc # player
+				yt-dlg # download graphically
+				yt-dlp # download from cli
+				mpv-replace
+			];
 
-		# programs.mpv = {
-		# 	enable = true;
-		#
-		# 	package = (
-		# 		pkgs.mpv-unwrapped.wrapper {
-		# 			scripts = with pkgs.mpvScripts; [
-		# 				cut
-		# 			];
-		#
-		# 			mpv =
-		# 				pkgs.mpv-unwrapped.override {
-		# 					waylandSupport = true;
-		# 				};
-		# 		}
-		# 	);
-		#
-		# 	config = {
-		# 		profile = "high-quality";
-		# 		ytdl-format = "bestvideo+bestaudio";
-		# 		cache-default = 4000000;
-		# 	};
+		programs.mpv = {
+			enable = true;
+
+			scripts = with pkgs.mpvScripts; [
+				cut
+			];
+
+			config = {
+				idle = "no";
+				keep-open = "always";
+				profile = "gpu-hq";
+				save-position-on-quit = "no";
+				ytdl-format = "bestvideo+bestaudio";
+				# cache-default = 4000000;
+			};
+		};
+
+		# xdg.desktopEntries.mpv-replace = {
+		# 	name = "mpv-replace";
+		# 	exec = "mpv --input-ipc-server=/tmp/mpv.sock %U";
+		# 	mimeType = [
+		# 		"video/mp4"
+		# 		"video/x-matroska"
+		# 		"video/webm"
+		# 		"video/quicktime"
+		# 	];
 		# };
 	};
 }
