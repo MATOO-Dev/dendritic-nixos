15d0857978e919cbd52898a340edd9cb0b905ba7
MATOO-Dev <matoo-dev@proton.me>
2026-07-09 22:22:36 +0200

    Removed unity and vs code

--- modules/features/coding.nix
+++ modules/features/coding.nix
@@ -8,7 +8,6 @@
 			cargo-deny # cargo denpendency linting / license checking
 			clang # clang compiler
 			clang-tools # additional tools for clang
-			dotnet-sdk_9 # c# sdk
 			gdb # c++ / rust compiler
 			unstable.godot # game engine
 			unstable.material-maker # PBR material tool
@@ -16,22 +15,7 @@
 			rustc # rust compiler
 			rustlings # rust beginner exercises
 			scons # c++ meta build tool
-			unityhub # game engine
 			valgrind # c++ memory profiler
-			omnisharp-roslyn
-			netcoredbg
-			vscode-fhs
-			(vscode-with-extensions.override {
-					vscode = vscodium;
-					vscodeExtensions = with vscode-extensions; [
-						catppuccin.catppuccin-vsc # theme
-						# ms-dotnettools.csharp # C# integration
-						# ms-dotnettools.vscode-dotnet-runtime # .net integration
-						usernamehw.errorlens # inline errors
-						visualstudiotoolsforunity.vstuc # unity integration
-						vscodevim.vim # vim bindings
-					];
-				})
 		];
 	};
 }
